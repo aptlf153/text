9bdef1e1fc68a759ed6435eb0b6fc01069c9c112
kimhyunsun <aptlf153@naver.com>
2025-03-08 10:47:05 +0900

    vite react firebase git

--- src/App.jsx
+++ src/App.jsx
@@ -6,28 +6,16 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const handleClick = () => {
+    alert(1); // 얼럿창에 1을 띄움
+  };
+
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>ID<input type="text" /></div>
+      <div>password<input type="text" /></div>  
+      <div><button onClick={handleClick}>회원가인</button></div>
+      <div><button>로그인</button></div>
     </>
   )
 }
